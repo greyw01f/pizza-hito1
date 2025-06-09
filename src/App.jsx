2d4b9e04e326d7cec84ab5c9cbcb8ad6ca82738b
greyw01f <83853649+greyw01f@users.noreply.github.com>
2025-06-08 22:28:17 -0400

    nueva app hito dos App.jsx
    Se actualizó la app jsx con los requerimietos hito 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,21 @@ import './App.css'
 import Footer from './components/Footer'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
+import Register from './components/Register';
+import Login from './components/Login';
 
 function App() {
-  
-
   return (
     <>
-     <Navbar />
-     <Home />
-     <Footer />
+      <Navbar />
+      {/* <Home /> */}{" "}
+      
+      <Register /> 
+      {/* <Login /> */}{" "}
+     
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
