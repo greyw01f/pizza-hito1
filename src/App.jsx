177b3a36a3ecd6814e402d72507e3207747f5122
greyw01f <83853649+greyw01f@users.noreply.github.com>
2025-07-06 22:52:39 -0400

    Update App.jsx
    hito 6

--- src/App.jsx
+++ src/App.jsx
@@ -14,21 +14,25 @@ import NotFound from './pages/NotFound';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
+import { CartProvider } from './context/CartContext'; // Importa el contexto del carrito
+
 function App() {
   return (
     
     <BrowserRouter>
-      <Navbar /> {/* Navbar siempre visible */}
-      <Routes> {/* Define las rutas de la aplicación [cite: 17] */}
-        <Route path="/" element={<Home />} /> {/* Ruta para Home [cite: 18] */}
-        <Route path="/register" element={<Register />} /> {/* Ruta para Register [cite: 21] */}
-        <Route path="/login" element={<Login />} /> {/* Ruta para Login [cite: 22] */}
-        <Route path="/cart" element={<Cart />} /> {/* Ruta para Cart [cite: 23] */}
-        <Route path="/pizza/p001" element={<Pizza />} /> {/* Ruta para Pizza (ID fijo por ahora) [cite: 24] */}
-        <Route path="/profile" element={<Profile />} /> {/* Ruta para Profile [cite: 25] */}
-        <Route path="*" element={<NotFound />} /> {/* Ruta para NotFound (cualquier otra ruta) [cite: 25, 26] */}
-      </Routes>
-      <Footer /> {/* Footer siempre visible */}
+      <CartProvider> {/* Proveedor del contexto del carrito [cite: 16] */}
+        <Navbar /> {/* Navbar siempre visible */}
+        <Routes> {/* Define las rutas de la aplicación [cite: 17] */}
+          <Route path="/" element={<Home />} /> {/* Ruta para Home [cite: 18] */}
+          <Route path="/register" element={<Register />} /> {/* Ruta para Register [cite: 21] */}
+          <Route path="/login" element={<Login />} /> {/* Ruta para Login [cite: 22] */}
+          <Route path="/cart" element={<Cart />} /> {/* Ruta para Cart [cite: 23] */}
+          <Route path="/pizza/p001" element={<Pizza />} /> {/* Ruta para Pizza (ID fijo por ahora) [cite: 24] */}
+          <Route path="/profile" element={<Profile />} /> {/* Ruta para Profile [cite: 25] */}
+          <Route path="*" element={<NotFound />} /> {/* Ruta para NotFound (cualquier otra ruta) [cite: 25, 26] */}
+        </Routes>
+        <Footer /> {/* Footer siempre visible */}
+      </CartProvider>
     </BrowserRouter>
   );
 }
